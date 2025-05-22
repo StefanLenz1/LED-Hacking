185dc0d670e901cd627ffa8bc6eefc9c59a4d756
haennes <hannes.hofmuth@gmail.com>
2025-05-23 01:05:11 +0200

    add codeview component

---
+++ website/src/components/code_view.rs
@@ -0,0 +1,15 @@
+use crate::components::*;
+use crate::error_template::{AppError, ErrorTemplate};
+use leptos::*;
+use leptos::*;
+use leptos::*;
+use leptos_meta::*;
+use leptos_meta::*;
+use leptos_router::*;
+use leptos_router::*;
+#[component]
+pub fn CodeView() -> impl IntoView {
+    view! {
+        <div> some code view here </div>
+    }
+}
