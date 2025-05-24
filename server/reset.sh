de70ef85b0007a88e3bee72035c83e3447a54426
pi <pi@example.com>
2025-05-24 11:05:42 +0100

    last fixes

--- server/reset.sh
+++ server/reset.sh
@@ -1,1 +1,2 @@
-cp ~/usercode.cpp /home/zwie/work/ESP-LSD-Hacking/src/task1/accessable/usercode.cpp > /tmp/logs25
+cp /home/zwie//usercode.cpp /home/zwie/work/ESP-LSD-Hacking/src/task1/accessable/usercode.cpp > /tmp/logs25
+./upload.sh
